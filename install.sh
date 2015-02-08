9441e5dea80f4a107f8412f8dc16fbece71ea84a
LogvinovLeon <leon@growbots.co>
2015-02-08 18:14:26 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+virtualenv venv
+. venv/bin/activate
+cd BD_polls/
+pip install -r requirements.txt
+export PYTHONPATH=$PYTHONPATH:$(pwd)/DB-project
+./manage.py makemigrations
+./manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
+./manage.py runserver 0.0.0.0:8000
